019c11f8777a8cf9aed70d2f3ad95ab4449033f4
doihnmaa <shishkina.alice@gmail.com>
2026-04-14 20:21:35 +0300

    створено фільтр. збереження в джсон. створено графік

--- assignment4-shyshkina.py
+++ assignment4-shyshkina.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("random_walk.csv")
-print(df.head())
+print(df.head()) #хед це перші 5 рядків таблиці. від 0 починається
 
 df["distance"] = np.sqrt(df["x"]**2 + df["y"]**2) #відстань векторно
 print(df.head())
@@ -12,3 +12,12 @@ max_distance = df["distance"].max() #максимальна відстань
 avg_distance = df["distance"].mean() #середня відстань
 print(f"\nмаксимальна відстань: {max_distance:.2f}")
 print(f"середня відстань: {avg_distance:.2f}")
+
+filtered = df[df["distance"] > avg_distance] #відстань до центру є більшою, ніж середнє значення
+print(filtered)
+
+# filtered.to_json("filtered_walk.json", orient="records", indent=4) #береження джсон
+
+plt.figure(figsize=(8, 4))
+plt.plot(df["x"], df["y"], color="purple")
+plt.show()
